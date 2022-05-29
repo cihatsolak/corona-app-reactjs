2a8d1759c0c33a6238e57f6fb74fa94373ba2ae9
cihatsolak <cihatsolak@hotmail.com>
2022-05-29 21:01:59 +0300

    Show corona cases by continent in the table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Card from "./components/Card";
 import CoronaForCountry from "./components/CoronaForCountry";
 import RecordedCorona from "./components/RecordedCorona";
+import CoronaForContinent from "./components/CoronaForContinent";
 
 function App() {
   const [key, setKey] = useState(1);
@@ -22,6 +23,10 @@ function App() {
 
         {
           key === 2 && <RecordedCorona />
+        }
+
+{
+          key === 3 && <CoronaForContinent />
         }
       </div>
     </>
